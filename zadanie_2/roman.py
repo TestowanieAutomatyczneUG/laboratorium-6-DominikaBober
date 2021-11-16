0bf688ea2a157f83ea5dfd0cdfdb76921877bc98
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2021-11-16 09:49:24 +0100

    simplification of roman function

--- zadanie_2/roman.py
+++ zadanie_2/roman.py
@@ -6,32 +6,19 @@ def roman(number):
         setki = int((number%1000 - dziesiatki - jedności)/100)
         tysiace = int((number%10000 -setki - dziesiatki - jedności)/1000)
 
-        if jedności<4:
-            roman_number = jedności*"I"
-        elif jedności==4:
-            roman_number = "IV"
-        elif jedności<9:
-            roman_number = "V" + (jedności-5)*"I"
-        else:
-            roman_number = "IX"
+        znaki = {"IVX": jedności, "XLC": dziesiatki, "CDM": setki}
+        roman_number = ""
+
+        for cykl in znaki:
         
-        if dziesiatki<4:
-            roman_number =  dziesiatki*"X" + roman_number
-        elif dziesiatki==4:
-            roman_number = "XL" + roman_number
-        elif dziesiatki<9:
-            roman_number = "L" + (dziesiatki-5)*"X" + roman_number
-        else:
-            roman_number = "XC" + roman_number
-        
-        if setki<4:
-            roman_number =  setki*"C" + roman_number
-        elif setki==4:
-            roman_number = "CD" + roman_number
-        elif setki<9:
-            roman_number = "D" + (setki-5)*"C" + roman_number
-        else:
-            roman_number = "CM" + roman_number
+            if znaki[cykl]<4:
+                roman_number =  znaki[cykl]*cykl[0] + roman_number
+            elif znaki[cykl]==4:
+                roman_number = cykl[:2] + roman_number
+            elif znaki[cykl]<9:
+                roman_number = cykl[1] + (znaki[cykl]-5)*cykl[0] + roman_number
+            else:
+                roman_number = cykl[0] + cykl[2] + roman_number
 
         roman_number = tysiace*"M" + roman_number
 
