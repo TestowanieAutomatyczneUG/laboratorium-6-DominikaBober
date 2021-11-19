fd1a4a663d36ea7c6989e9f10903d8b5c257cfec
DominikaBober <75113657+DominikaBober@users.noreply.github.com>
2021-11-19 09:44:11 +0100

    TDD test long gens

--- zadanie_1/hamming.py
+++ zadanie_1/hamming.py
@@ -1,5 +1,10 @@
 class Hamming():
 
     def distance(self, var_1, var_2):
+
+        result = 0
+
+        for i in range(len(var_1)):
+            result = result + 1-(var_1[i]==var_2[i])
         
-        return 1-(var_1==var_2)
+        return result
